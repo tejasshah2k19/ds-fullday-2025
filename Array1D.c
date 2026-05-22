3b9c2eebb5ae6089846813ae1856518fb207a0d6
Tejas Shah <tejasshah2k19@gmail.com>
2026-05-22 13:06:28 +0530

    array insert

--- Array1D.c
+++ Array1D.c
@@ -1,35 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
+#define SIZE 5
 
+// function
 
-//function 
+// inputArray
 
-//inputArray 
+// printArray
 
-//printArray 
+int arr[SIZE]; // extern
 
+void insert(int location, int data)
+{
+    int index = location - 1;
+    int j; 
 
+    if (arr[index] == 0)
+    {
+        arr[index] = data;
+    }
+    else
+    {
+        //index -> already data inserted 
+        
+        for(j=SIZE-1; j != index  ;j-- ){
+            arr[j] = arr[j-1];
+        }
 
-void insert(int location,int data){
-
+        arr[index] = data; 
+    }
 }
 
-int main(){
+int main()
+{
 
-    int arr[5];
-    int i; 
- 
+    int i;
 
-    insert(1,10);//0
-    insert(2,20);//1
-    insert(1,30);//0
-    insert(4,40);//3 
-    insert(2,50);//1
-    
+    insert(1, 10); // 0  10
+    insert(2, 20); // 0  10
+    insert(3, 30); // 0  10
+    insert(4, 40); // 0  10
+    insert(5, 50); // 0  10
 
     printf("\nElements in Array : ");
-    for(i=0;i<5;i++){
-        printf(" %d",arr[i]); 
+    for (i = 0; i < 5; i++)
+    {
+        printf(" %d", arr[i]);
     }
 
-    return 0; 
+    return 0;
 }
