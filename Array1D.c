d8ae95a2abf016d94415c46ae7eb62e2e722c816
Tejas Shah <tejasshah2k19@gmail.com>
2026-05-22 12:17:29 +0530

    array

---
+++ Array1D.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+//function 
+
+//inputArray 
+
+//printArray 
+
+
+
+void insert(int location,int data){
+
+}
+
+int main(){
+
+    int arr[5];
+    int i; 
+ 
+
+    insert(1,10);//0
+    insert(2,20);//1
+    insert(1,30);//0
+    insert(4,40);//3 
+    insert(2,50);//1
+    
+
+    printf("\nElements in Array : ");
+    for(i=0;i<5;i++){
+        printf(" %d",arr[i]); 
+    }
+
+    return 0; 
+}
