f3110cad5712ce84065b8b5473d93457ed907584
Tejas Shah <tejasshah2k19@gmail.com>
2026-05-25 12:00:10 +0530

    array deletion , linear search

--- Array1D.c
+++ Array1D.c
@@ -12,7 +12,7 @@ int arr[SIZE]; // extern
 void insert(int location, int data)
 {
     int index = location - 1;
-    int j; 
+    int j;
 
     if (arr[index] == 0)
     {
@@ -20,13 +20,47 @@ void insert(int location, int data)
     }
     else
     {
-        //index -> already data inserted 
-        
-        for(j=SIZE-1; j != index  ;j-- ){
-            arr[j] = arr[j-1];
+        // index -> already data inserted
+
+        for (j = SIZE - 1; j != index; j--)
+        {
+            arr[j] = arr[j - 1];
         }
 
-        arr[index] = data; 
+        arr[index] = data;
+    }
+}
+
+void removeArray(int location)
+{
+    int i;
+
+    for (i = location - 1; i < SIZE - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    arr[SIZE - 1] = 0;
+}
+
+void linearSearch(int key)
+{
+    // flag
+    int i;
+    int found = -1; // not found
+    for (i = 0; i < SIZE; i++)
+    {
+
+        if (arr[i] == key)
+        { // 50 == 50
+            printf("%d Found", key);
+            found = 1; // found
+            break;
+        }
+    }
+    if (found == -1)
+    {
+        printf("%d Not Found ", key); // 50 not found
     }
 }
 
@@ -46,6 +80,16 @@ int main()
     {
         printf(" %d", arr[i]);
     }
+
+    removeArray(1);
+
+    printf("\nElements in Array : ");
+    for (i = 0; i < 5; i++)
+    {
+        printf(" %d", arr[i]);
+    }
+
+    linearSearch(50);
 
     return 0;
 }
